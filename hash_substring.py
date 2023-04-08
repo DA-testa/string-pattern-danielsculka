b42728c6d568e2e1cfa46263359ed05a4e54c653
Dāniels Čulka <daniels.culka@edu.rtu.lv>
2023-04-09 00:52:45 +0300

    test

--- hash_substring.py
+++ hash_substring.py
@@ -1,29 +1,48 @@
 # python3
+# Dāniels Čulka 221RDB304
+
+B = 13
+
+def get_hash(text: str) -> int:
+    hash = 0
+    for i in range(1, len(text)):
+        hash += ord(text[i - 1]) * B**(len(text) - i)
+        
+    return hash
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    inputType = input()
+
+    while 'I' not in inputType or 'F' not in inputType:
+        if 'I' in inputType:
+            return (input().rstrip(), input().rstrip())
+        elif 'F' in inputType:
+            fileName = input()
+            
+            with open("./tests/%s" % (fileName), "r") as file:
+                return (file.readline().rstrip(), file.readline().rstrip())
+        
+        inputType = input()
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    pattern_len = len(pattern)
+    pattern_hash = get_hash(pattern)
+    text_len = len(text)
 
-    # and return an iterable variable
-    return [0]
+    occurrences = []
+    for i in range(0, text_len - pattern_len + 1):
+        text_part = text[i:i + pattern_len]
+        text_part_hash = get_hash(text_part)
+
+        if pattern_hash == text_part_hash:
+            if pattern == text_part:
+                occurrences.append(i)
+
+    return occurrences
 
 
 # this part launches the functions
